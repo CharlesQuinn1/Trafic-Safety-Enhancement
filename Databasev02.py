1507b888db03cc2ece84cffa2829aabf0e13da6c
CharlesQuinn1 <128498023+CharlesQuinn1@users.noreply.github.com>
2023-08-24 14:01:43 -0500

    Update files

--- Databasev02.py
+++ Databasev02.py
@@ -3,10 +3,16 @@ import os
 import sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, String, Float, MetaData, Table
 
-#####################################
-# Create trafficdb in postgres first
-#####################################
+#################################################
+# 1. Create trafficdb in local postgresDB first
+# 2. Run below code to:
+#     a. Read csv file
+#     b. Transform data
+#     c. Create traffic_data table in trafficdb
+#     d. Load table
+#################################################
 
+# Establish file path
 dpath = 'Trafic-Safety-Enhancement/data/'
 dbpath = 'Trafic-Safety-Enhancement/'
 dfile = 'Real-Time_Traffic_Incident_Reports.csv'
@@ -16,6 +22,8 @@ gdatapath = os.path.join(dpath, gfile)
 
 # import data from csv
 trafficData_df = pd.read_csv(datapath)
+
+# transform data
 trafficData_df['issue_reported'] = trafficData_df['issue_reported'].str.upper()
 trafficData_df['id'] = trafficData_df.index
 trafficData_df.drop(columns=['traffic_report_id'], inplace=True)
@@ -23,29 +31,18 @@ trafficData_df['published_date'] = pd.to_datetime(trafficData_df['published_date
 trafficData_df = trafficData_df[['id','published_date','issue_reported','location',
                                 'latitude','longitude','address']]
 
+# Remove invalid coordinates
 trafficData_df = trafficData_df.loc[~trafficData_df['latitude'].isna() | ~trafficData_df['longitude'].isna()]
 
-# print(trafficData_df.head())
-
-
+# Create connection to local postgres database
 engine_url = "postgresql://postgres:postgres@localhost:5432/trafficdb"
 engine = create_engine(engine_url)
 conn = engine.connect() 
 metadata = MetaData(engine)
 
+# Create table and load data
 exist=sqlalchemy.inspect(engine).has_table("traffic_data")
 if exist == False:
-    print("Table doesn't exist")
-    # traffic = Table('traffic_data', metadata,
-    #             Column('id', Integer(), primary_key=True),
-    #             Column('published_date', String(255), nullable=False),
-    #             Column('location', String(255)),
-    #             Column('latitude', Float),
-    #             Column('longitude', Float),
-    #             Column('address', String(255))
-    #             )
-    # metadata.create_all(engine)
-    # print("Table was created")
     trafficData_df.to_sql('traffic_data', engine)
     print("Table loaded")
 else:
