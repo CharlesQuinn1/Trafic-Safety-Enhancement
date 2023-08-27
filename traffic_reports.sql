4c3a57ccf1f438eeede2beb9c79b4cc65257190c
CharlesQuinn1 <128498023+CharlesQuinn1@users.noreply.github.com>
2023-08-27 14:48:15 -0500

    Update files

--- traffic_reports.sql
+++ traffic_reports.sql
@@ -28,3 +28,79 @@ WHERE
 DELETE FROM traffic_report
 WHERE
     published_date::timestamp <= '2017-12-31 24:00:00'::timestamp;
+
+CREATE TABLE ISSUE_CAT AS
+select
+ t1.issue_reported
+,case
+	when 
+	upper(t1.issue_reported) like 'TRAFFIC%' OR
+	upper(t1.issue_reported) like 'AUTO%' OR
+	upper(t1.issue_reported) like 'BLOCK%' OR
+	upper(t1.issue_reported) like '%TRFC%' OR
+	upper(t1.issue_reported) like 'OBST%'
+	then 'OBSTRUCTION'
+	when
+	upper(t1.issue_reported) like 'COLLI%' OR
+	upper(t1.issue_reported) like 'CRASH%' OR
+	upper(t1.issue_reported) like 'FLEET%'
+	then 'COLLISION'
+	when
+	upper(t1.issue_reported) like 'COLLI%' OR
+	upper(t1.issue_reported) like 'CRASH%' OR
+	upper(t1.issue_reported) like 'FLEET%'
+	then 'COLLISION'
+	when
+	upper(t1.issue_reported) like 'HIGH%'
+	then 'HIGH WATER'
+	when
+	upper(t1.issue_reported) like 'ICY%'
+	then 'ICY ROADWAY'
+	when
+	upper(t1.issue_reported) like '%STALL%'
+	then 'STALLED VEHICLE'
+	when
+	upper(t1.issue_reported) like '%BOAT%'
+	then 'BOAT ACCIDENT'
+	when
+	upper(t1.issue_reported) like '%LIVESTOCK%'
+	then 'LOOSE LIVESTOCK'
+	when
+	upper(t1.issue_reported) like '%FIRE%'
+	then 'VEHICLE FIRE'
+	end as issue_new
+from traffic_report t1
+group by
+ t1.issue_reported
+,case
+	when 
+	upper(t1.issue_reported) like 'TRAFFIC%' OR
+	upper(t1.issue_reported) like 'AUTO%' OR
+	upper(t1.issue_reported) like 'BLOCK%' OR
+	upper(t1.issue_reported) like 'TRFC%' OR
+	upper(t1.issue_reported) like 'OBST%'
+	then 'OBSTRUCTION'
+	when
+	upper(t1.issue_reported) like 'COLLI%' OR
+	upper(t1.issue_reported) like 'CRASH%' OR
+	upper(t1.issue_reported) like 'FLEET%'
+	then 'COLLISION'
+	when
+	upper(t1.issue_reported) like 'HIGH%'
+	then 'HIGH WATER'
+	when
+	upper(t1.issue_reported) like 'ICY%'
+	then 'ICY ROADWAY'
+	when
+	upper(t1.issue_reported) like '%STALL%'
+	then 'STALLED VEHICLE'
+	when
+	upper(t1.issue_reported) like '%BOAT%'
+	then 'BOAT ACCIDENT'
+	when
+	upper(t1.issue_reported) like '%LIVESTOCK%'
+	then 'LOOSE LIVESTOCK'
+	when
+	upper(t1.issue_reported) like '%FIRE%'
+	then 'VEHICLE FIRE'
+	end
